da8d1fc3a8d958fb4bf2d0c32c1d5c1fe0a83ee3
Thiird <47575923+Thiird@users.noreply.github.com>
2022-03-29 23:59:40 +0200

    FIxes, added datasheets

--- BL55077_lcd.ino
+++ BL55077_lcd.ino
@@ -1,47 +1,56 @@
 #include <Wire.h>
 
 // i2c
-#define SCL_PIN 3
-#define SDA_PIN 2
-#define LCD_SAO 0b0
-#define LCD_DRIVER_SUBADDR 0 << 2
-#define LCD_DRIVER_ADDR 0b011111 << 2 | LCD_SAO << 1 | 0b0
+#define SAO 0b0
+#define DRIVER_SUBADDR 0 << 2
+#define DRIVER_ADDR_77 0b00111000 // BL55077
+#define DRIVER_ADDR_66 0b00111110 // BL55066
 
 // driver config
-#define LCD_BIAS 0b0 // 0 = 1/3, 1 = 1/2
-#define LCD_ENABLED 0b1
-#define LCD_PWR_MODE 0b0 // 0 normal, 1 power-saving
+#define BIAS 0b0 // 0 = 1/3, 1 = 1/2
+#define ENABLED 0b1
+#define PWR_MODE 0b0 // 0 normal, 1 power-saving
 #define BLINK_MODE 0b0 // 0 = normal blinking, 1 = alternate blinking
 
-int cont = 0;
-byte device_select_cmd = 0b0; // reset command
-byte bank_select_cmd = 0b0;
+byte driver_config = 0;
+byte device_select_cmd = 0;
+byte bank_select_cmd = 0;
 byte display_data = 0b0;
 byte current_bank = 0b0;
+byte ram_addr = 0;
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin();
+  while (!Serial);
+  Serial.println("---------------");
+  
+  // display setup 0b0 << 7 | 0b10 << 5 | LCD_PWR_MODE << 4 | LCD_ENABLED << 3 | LCD_BIAS << 2 | 0b1;
+  driver_config = 0b11001001;
+  device_select_cmd = 0b11100000; //1 << 7 | 0b1100 << 3 | LCD_DRIVER_SUBADDR;
+  bank_select_cmd = 1 << 7 | 0b11110 << 2 | 00 << 1;
 
-  // display setup
-  byte driver_config = 0 << 7 | 10 << 5 | LCD_PWR_MODE << 4 | LCD_ENABLED << 3 | LCD_BIAS << 2;
-  Wire.beginTransmission(LCD_DRIVER_ADDR);
+  Wire.begin();
+  
+  Wire.beginTransmission(DRIVER_ADDR_66);
   Wire.write(driver_config);
+  Wire.write(device_select_cmd);
+  Wire.write(bank_select_cmd);
   Wire.endTransmission();
+  Serial.println("Setup Done");
 }
 
 void loop() {
 
-  // build commands 
-  byte device_select_cmd = 1 << 7 | 0b1100 << 3 | LCD_DRIVER_SUBADDR;  
-  byte bank_select_cmd = 0 << 7 | 0b11110 << 2 | 00 << 1;
-  byte display_data = 0b11111111;
+  // build commands
+  display_data = 0b11111111;
    
-  Wire.beginTransmission(LCD_DRIVER_ADDR);
-  Wire.write(device_select_cmd);
-  Wire.write(bank_select_cmd);
+  Wire.beginTransmission(DRIVER_ADDR_66);
+  Wire.write(ram_addr);
   Wire.write(display_data);
   Wire.endTransmission();
-  delay(1);
+  Serial.println("Sending data ");
+  delay(500);
 
+  if (ram_addr < 250){ram_addr++;}
+  else { ram_addr = 0; }
 }
